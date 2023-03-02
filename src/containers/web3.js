2366c9f096ac939df7be38027018e63589ea15b1
admin <2642438745@qq.com>
2023-03-03 00:10:24 +0800

    feat: aa

--- src/containers/web3.js
+++ src/containers/web3.js
@@ -1,7 +1,15 @@
 import React, {useState} from 'react';
 const Web3 = require('web3');
-import '../style/web3.css'
+import '../style/web3.css';
+import contractsApi from '../utils/contracts.json';
+let networks = new Object(contractsApi.networks);
+// for (const key of networks) {
+//     console.log('key',key);
+// }
+
 function WebThree () {
+    
+    console.log('list', typeof contractsApi.networks);
     let web3 = new Web3(Web3.givenProvider || 'ws://localhost:8545');
     web3.eth.requestAccounts().then(res => {
         console.log(res);
@@ -9,13 +17,14 @@ function WebThree () {
     const [blockNum, setBlockNum ] = useState(0);
     const [id, setId] = useState(1);
     const [balance, setBalance] = useState(1);
+    const [contractApi, setContractApi] = useState(null);
     web3.eth.getBlockNumber().then(res => {
         setBlockNum(res);
     });
     web3.eth.getChainId().then(res => {
         setId(res);
     });
-    web3.eth.getBalance("0x36953b5cB783E80275250ca9708090d7E63742C9").then(res => {
+    web3.eth.getBalance("0x8Ce73B72Cc5d76Ac50D233c9193a54A8Ed1779e5").then(res => {
         console.log(res);
         setBalance(web3.utils.fromWei(res, 'ether'));
     });
@@ -24,17 +33,32 @@ function WebThree () {
         console.log(res);
     });
     function conntectFox(params) {
+        web3.eth.requestAccounts().then(res => {
+            console.log(res);
+        });
+    }
+    function sendTransaction() {
         web3.eth.sendTransaction({
-            from: '0x36953b5cB783E80275250ca9708090d7E63742C9',
-            to: '0x21Ab88105E1ea1E9904C16a2BA21144786994764',
+            from: '0x8Ce73B72Cc5d76Ac50D233c9193a54A8Ed1779e5',
+            to: '0x06E83b9F0B76b6A79B596C82b05CCb139a2Ce959',
             value: web3.utils.toWei('100', 'ether')
         })
         .then(function(receipt){
-            web3.eth.getBalance("0x36953b5cB783E80275250ca9708090d7E63742C9").then(res => {
+            web3.eth.getBalance("0x8Ce73B72Cc5d76Ac50D233c9193a54A8Ed1779e5").then(res => {
                 console.log(res);
-                setBalance(web3.utils.fromWei(res, 'etsher'));
+                setBalance(web3.utils.fromWei(res, 'ether'));
             });
         });
+    }
+    async function connetContract() {
+        web3.eth.defaultAccount = '0x220422E9ccbc84db42B17faD9f1cA4E924a92BEc';
+        let contractData = await new web3.eth.Contract(contractsApi.abi, '0x4f78F062635166B4B9e8F96402440E8f2a93e84f');
+        setContractApi(contractData);
+        console.log('contractData', contractData);
+    }
+    function methdsApi() {
+        console.log('asd', web3.eth.defaultAccount);
+        contractApi.methods.setStudents('lc', 18).send();
     }
     return (
         <div>
@@ -43,6 +67,15 @@ function WebThree () {
             </div>
             <div className='web3-main' onClick={conntectFox}>
                 <button>连接小狐狸</button>
+            </div>
+            <div className='web3-main' onClick={sendTransaction}>
+                <button>发送交易</button>
+            </div>
+            <div className='web3-main' onClick={connetContract}>
+                <button>连接合约</button>
+            </div>
+            <div className='web3-main' onClick={methdsApi}>
+                <button>调用合约方法</button>
             </div>
             <div>当前区块{blockNum}</div>
             <div>当前链id{id}</div>
